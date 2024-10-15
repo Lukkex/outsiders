b795450395db8ad9885785cbe93024280a72d0ce
Lukkex <87504872+Lukkex@users.noreply.github.com>
2024-10-14 21:49:22 -0700

    React Framework Setup!
    Jira AA-86

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Outsiders Football Club for Folsom and San Quentin!</h1>
+
+      <div class="horizontalcontainer"> 
+
+      <div class="headerbuttoncontainer">
+        <p text-align="center"><strong>HOME</strong></p>
+      </div>
+
+      <div class="headerbuttoncontainer">
+        <p text-align="center">Registration</p>
+      </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
