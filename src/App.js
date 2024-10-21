e38b3dedbd5dee9c975e7e7e6a2bb4f4bfa97d3e
williamlorence <williamlorence@csus.edu>
2024-10-20 19:22:31 -0700

    added the logo file to the project directory

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {View} from 'react-native';
 function App() {
   return (
     <div className="App">
-      <img src="https://cdn.discordapp.com/attachments/1277835451709001748/1295613457638621184/outsiders4.png?ex=67148f8f&is=67133e0f&hm=0b35e213d338ce801d28499ba12804066b94bcea74d9a7194e1013e2a4c65db6&" alt="Logo"></img>
+      <img src="../images/outsiders4.png" alt="Logo"></img>
       <h1>Welcome to Outsiders Football Club for Folsom and San Quentin!</h1>
       <div class="site-header">
         <button class="rounded-button">HOME</button>
