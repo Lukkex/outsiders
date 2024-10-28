65fea9942b8f563bba17221f4c9caf379e19ef93
Wen0131 <144382637+Wen0131@users.noreply.github.com>
2024-10-28 01:44:53 -0700

    S3 and DynamoDB setup complete.
    Removed .aws/credentials
    
    updated .env file, removed sensitive info

---
+++ backend/awsServices/dynamoDB.js
@@ -0,0 +1,52 @@
+import { GetItemCommand, PutItemCommand } from '@aws-sdk/client-dynamodb';
+import { dynamoDB } from '../awsConfig.js';
+
+const tableName = process.env.DYNAMODB_USER_TABLE;
+
+async function checkUserExists(userID) {
+    const params = {
+        TableName: tableName,
+        Key: {
+            userID: { S: userID} //the S specify attribute type as string, capitalized
+        }
+    };
+    
+    try {
+        const command = new GetItemCommand(params);
+        const data = await dynamoDB.send(command);
+        return !!data.Item;
+        /*
+        if(data.Item) {
+            return true //user exists in database
+        }
+        else return false; //user does not exists, then process
+        */
+    } catch (error) {
+        console.error("Error checking if user exists:", error);
+        throw error;
+    }
+}
+
+async function addUserToTable(userID, firstName, lastname, email) {
+    const params = {
+        TableName: tableName,
+        Item: {
+            userID: { S: userID},
+            firstName: { S: firstName},
+            lastname: { S: lastname},
+            email: { S: email}
+        }
+    };
+
+    try {
+        const command = new PutItemCommand(params);
+        const data = await dynamoDB.send(command);
+        return data;
+    } catch (error) {
+        console.error("Error adding user data to DynamoDB:", error)
+        throw error;
+    }
+}
+
+export { checkUserExists, addUserToTable };
+
