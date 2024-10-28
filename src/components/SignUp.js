3af16dad5eaae1f833c70e7e1d81ac4c10032774
Christian Young <xian02young@yahoo.com>
2024-10-27 17:33:30 -0700

    Signup.js and .css

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,6 @@
 import '../App.css';
-import { useNavigate } from "react-router-dom";
+import '../SignUp.css';
+import { useNavigate, Link } from "react-router-dom";
 
 function SignUp() {
     const navigate = useNavigate();
@@ -11,21 +12,44 @@ function SignUp() {
         navigate('/');
     };
     
-    //Page content
-    //*** No CSS yet 
     return (
-      <div>
-        <img src="../images/outsiders4.png" alt="Logo"></img>
-        <h1>Sign up page</h1>
-        <div>
-          <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-            <button type="submit">Sign Up</button>
-          </form>
+        <div className="Home">
+            <img src="../images/outsiders4.png" alt="Logo" />
+            <div className="site-header">
+                <Link to="/">
+                    <button className="rounded-button">HOME</button>
+                </Link>
+                <Link to="/registration">
+                    <button className="rounded-button">REGISTRATION</button>
+                </Link>
+                <Link to="/scheduling">
+                    <button className="rounded-button">SCHEDULING</button>
+                </Link>
+                <div className="dropdown">
+                    <button className="rounded-button dropdown-button">ACCOUNT</button>
+                    <div className="dropdown-content">
+                        <Link to="/signin">Sign In</Link>
+                        <Link to="/signup">Sign Up</Link>
+                    </div>
+                </div>
+            </div>
+            <div className="signup-container">
+                <h1>Create Your Account</h1>
+                <div className="form-container">
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" placeholder="First Name" className="form-input" />
+                        <input type="text" placeholder="Last Name" className="form-input" />
+                        <input type="text" placeholder="Username" className="form-input" />
+                        <input type="email" placeholder="Email" className="form-input" />
+                        <input type="password" placeholder="Password" className="form-input" />
+                        <input type="password" placeholder="Confirm Password" className="form-input" />
+                        <button type="submit" className="rounded-button">Sign Up</button>
+                    </form>
+                    <p>Already have an account? <Link to="/signin">Sign in here</Link>.</p>
+                </div>
+            </div>
         </div>
-      </div>
     );
-  }
-  
-  export default SignUp;
+}
+
+export default SignUp;
