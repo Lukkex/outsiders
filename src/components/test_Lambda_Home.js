f6ce2206a60f263ea52e5a2386a2bd1f8b11bfa0
Trenton Suddaby <suddabyt@gmail.com>
2024-10-30 00:25:20 -0700

    Attempt at updating lambda code

--- src/components/test_Lambda_Home.js
+++ src/components/test_Lambda_Home.js
@@ -2,7 +2,7 @@ export const handler = async (event) => {
     // TODO implement
     const response = {
       statusCode: 200,
-      body: JSON.stringify('Hello...Testing'),
+      body: JSON.stringify('Hello...Test'),
     };
     return response;
   };
