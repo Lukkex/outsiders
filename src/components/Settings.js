b56465e123b0635e84ac9bad3215ca9a5756ac27
qali03 <81342885+qali03@users.noreply.github.com>
2024-11-04 20:04:23 -0800

    Settings

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,12 +1,36 @@
 import '../App.css'; //global styles
 import SiteHeader from './SiteHeader';
 import TextBlackBG from './TextBlackBG';
+import { useNavigate, Link } from "react-router-dom";
 
 function Settings() {
+    const navigate = useNavigate();
+
+    //When users submit their sign up info, redirects to Landing page without rereshing
+    //*** No error checking atm: users can submit nothing, etc
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        navigate('/');
+    };
     return (
     <div>
         <SiteHeader></SiteHeader>
-        <TextBlackBG label="Settings Page"></TextBlackBG>
+        <br></br>
+            <br></br>
+            <br></br>
+            <div className="signin-container">
+                <h1 class="font-semibold">Settings</h1>
+                <div className="form-container">
+                    <form onSubmit={handleSubmit}>
+                        <button type="submit" className="rounded-button">Reset Passowrd</button>
+                        <button type="submit" className="rounded-button">Account Info</button>
+                        <button type="submit" className="rounded-button">Help</button>
+                        <br></br>
+                    </form>
+                    <br></br>
+                </div>
+            </div>
+        
     </div>
     );
 }
