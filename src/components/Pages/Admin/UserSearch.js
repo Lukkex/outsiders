46cce96c9d1851a0ae64aebf51e97cafe0647daf
Asephera <sonofgeek@hotmail.com>
2025-02-11 20:54:42 -0800

    Add CSS for adminscheduling, pagination for event table
    This is a WIP commit, more to be completed later.

--- src/components/Pages/Admin/UserSearch.js
+++ src/components/Pages/Admin/UserSearch.js
@@ -3,8 +3,9 @@
 * Notes: - This page uses dummy data. it updates within itself and queries locally.
 *        - Functions need updating for linked functionality
 * TODO: - Link Backend!
-*
-*
+*       - react table tree
+* KEY - ## = todo target
+*     - ##### = broad label
 */
 import React, { useState } from 'react';
 import {
@@ -16,28 +17,53 @@ import {
     Row,
     Cell,
 } from '@table-library/react-table-library/table';
+import { usePagination } from "@table-library/react-table-library/pagination";
+import { useTree } from '@table-library/react-table-library/tree';
 import styles from '../../Stylesheets/Scheduling.module.css';
 
 //## temp data struture, use to identify what info we want to retrieve/display instead of all user data
 const dummyUsers = [
-    { id: 1, firstName: 'Alice', lastName: 'Smith', location: 'Folsom', canAttend: true },
-    { id: 2, firstName: 'Bob', lastName: 'Jones', location: 'San Quentin', canAttend: false },
-    { id: 3, firstName: 'Smith', lastName: 'Jones', location: 'San Quentin', canAttend: true },
-    { id: 4, firstName: 'Jane', lastName: 'Smith', location: 'Folsom', canAttend: false },
+    { id: 1, firstName: 'Alice', lastName: 'Smith', nodes: [
+        {title: 'test1', location: 'Folsom', date: '2/14/2025', time: '10:00 AM', canAttend: true },
+        {title: 'test2', location: 'San Quentin', date: '2/24/2025', time: '11:00 AM', canAttend: true }
+    ],},
+    { id: 2, firstName: 'Bob', lastName: 'Jones', nodes: [
+        {title: 'test1', location: 'Folsom', date: '', time: '', canAttend: true },
+        {title: 'test2', location: 'Folsom', date: '', time: '', canAttend: true }
+    ],},
+    { id: 3, firstName: 'Smith', lastName: 'Jones', nodes: [
+        {title: 'test1', location: 'Folsom', date: '', time: '', canAttend: true },
+        {title: 'test2', location: 'Folsom', date: '', time: '', canAttend: true }
+    ],},
+    { id: 4, firstName: 'Jane', lastName: 'Smith', nodes: [
+        {title: 'test1', location: 'Folsom', date: '', time: '', canAttend: true },
+        {title: 'test2', location: 'Folsom', date: '', time: '', canAttend: true }
+    ],},
   ];
 
 const UserSearch = () => {
-    //variables
+    //##### variables
     const [searchQuery, setSearchQuery] = useState("");
     const [users, setUsers] = useState(dummyUsers); //import user data here
-    
-    //filter logic
+    const PAGELIMIT = 10;
+
+    // filter search logic
     const filteredUsers = users.filter((user) => //change to query backend for only matching users instead of parsing local data
         `${user.firstName} ${user.lastName}`.toLowerCase().includes(searchQuery.toLowerCase())
     );
 
-    //Table data format
+    //##### Table variables
     const data = {nodes: filteredUsers};
+    const tree = useTree(data, {
+        onChange: onTreeChange,
+    });
+
+    //##### functions
+
+    //currently just logs
+    function onTreeChange(action, state) {
+        console.log(action, state);
+    }
 
     const handleChange = (e) => {
         setSearchQuery(e.target.value);
@@ -52,10 +78,29 @@ const UserSearch = () => {
         );
     };
 
+    //##### table features
+
+    const pagination = usePagination(data, {
+          state: {
+            page: 0,
+            size: PAGELIMIT,
+          },
+          //onChange: onPaginationChange,
+        });
+    
+    /* ##later implement this to 'get pages' from the table as they are needed
+    function onPaginationChange(action, state) {
+    doGet({
+        offset: state.page * PAGELIMIT,
+        limit: PAGELIMIT,
+    });
+    } */
+
     return (
     <div>
-    <h2>User Search & RSVP Management</h2>
+        <h2>User Search & RSVP Management</h2>
         <div className={styles.tableContainer}>
+            <h2>User List</h2>
             <label htmlFor="search">Search User by Name: </label>
             <input //searchbar. Queries first/last/both
             type="text"
@@ -64,31 +109,39 @@ const UserSearch = () => {
             value={searchQuery}
             onChange={handleChange}
             />
-            <Table data={data}>
+            <Table className={styles.reactTable} data={data} pagination={pagination} tree={tree}>
                 {(tableList) => (
                     <>
-                    <Header className="header">
-                        <HeaderRow className="header-row">
-                            <HeaderCell className="header-cell">First Name</HeaderCell>
-                            <HeaderCell className="header-cell">Last Name</HeaderCell>
-                            <HeaderCell className="header-cell">Location</HeaderCell>
-                            <HeaderCell className="header-cell">RSVP</HeaderCell>
+                    <Header className={styles.header}>
+                        <HeaderRow className={styles.headerRow}>
+                            <HeaderCell resize className={styles.headerCell}>First Name</HeaderCell>
+                            <HeaderCell resize className={styles.headerCell}>Last Name</HeaderCell>
+                            <HeaderCell resize className={styles.headerCell}>Event</HeaderCell>
+                            <HeaderCell resize className={styles.headerCell}>Location</HeaderCell>
+                            <HeaderCell resize className={styles.headerCell}>Date & Time</HeaderCell>
+                            <HeaderCell resize className={styles.headerCell}>RSVP</HeaderCell>
                         </HeaderRow>
                     </Header>
 
                     <Body>
                         {tableList.map((item) => (
                             <Row key={item.id} item={item}>
-                                <Cell>{item.firstName}</Cell>
-                                <Cell>{item.lastName}</Cell>
-                                <Cell>{item.location}</Cell>
-                                <Cell>
+                                <Cell className={styles.cell}>{item.firstName}</Cell>
+                                <Cell className={styles.cell}>{item.lastName}</Cell>
+                                <Cell className={styles.cell}>{item.title}</Cell>
+                                <Cell className={styles.cell}>{item.location}</Cell>
+                                {(item.date || item.time) && (
+                                    <Cell className={styles.cell}>{item.date}, {item.time}</Cell>
+                                ) || (
+                                    <Cell className={styles.cell}></Cell>
+                                )}
+                                <Cell className={styles.cell}>
                                     <input
                                     type="checkbox"
                                     checked={item.canAttend}
                                     />
                                     <button onClick={() => handleRSVPChange(item.id)}>
-                                        {item.canAttend ? "-Cancel RSVP" : "-RSVP"}
+                                        {item.canAttend ? " -Cancel RSVP" : " -RSVP"}
                                     </button>
                                 </Cell>
                             </Row>
@@ -97,6 +150,18 @@ const UserSearch = () => {
                     </>
                 )}
             </Table>
+            Page:{' '}
+            {pagination.state.getPages(data.nodes).map((_, index) => (
+            <button
+                key={index}
+                type="button"
+                style={{
+                fontWeight: pagination.state.page === index ? 'bold' : 'normal',}}
+                onClick={() => pagination.fns.onSetPage(index)}
+            >
+                {index + 1}
+            </button>
+          ))}
         </div>
     </div>
     );
