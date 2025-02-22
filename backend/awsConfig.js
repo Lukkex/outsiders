8adfad8a57a04479835cbb1061803d09edf4e872
Wen0131 <144382637+Wen0131@users.noreply.github.com>
2025-02-21 16:14:45 -0800

    Save local changes before merging from main

--- backend/awsConfig.js
+++ backend/awsConfig.js
@@ -10,5 +10,12 @@ const dynamoDB = new DynamoDBClient({ region: process.env.AWS_REGION,
     credentials: {accessKeyId: process.env.AWS_ACCESS_KEY, 
                 secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY} });
 
-export {s3, dynamoDB};
+const awsCreds = {
+    aws_project_region: "us-west-1",
+    aws_cognito_region: "us-west-1",
+    aws_user_pools_id: "us-east-1_ATVGJk4CW",
+    aws_user_pools_web_client_id:"7jubasd14o6kf5co7vhvkavsbm"
+};
+
+export {s3, dynamoDB, awsCreds};
 
---
+++ backend/awsServices/awsCredentials.js
@@ -0,0 +1,8 @@
+const awsCreds = {
+    aws_project_region: "us-west-1",
+    aws_cognito_region: "us-west-1",
+    aws_user_pools_id: "us-east-1_ATVGJk4CW",
+    aws_user_pools_web_client_id:"7jubasd14o6kf5co7vhvkavsbm"
+};
+
+export default awsCreds;
