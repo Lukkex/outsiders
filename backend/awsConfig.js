c2a2656c0394f39f886bd4f779b2c9ea5d07cd1e
Wen0131 <144382637+Wen0131@users.noreply.github.com>
2025-03-08 14:28:48 -0800

    added DynamoDBDocumentClient to auto convert between JS object and DynamoDB format

--- backend/awsConfig.js
+++ backend/awsConfig.js
@@ -1,21 +1,12 @@
 import { S3Client } from "@aws-sdk/client-s3";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {DynamoDBDocumentClient} from "@aws-sdk/lib-dynamodb";
 import dotenv from "dotenv";
 dotenv.config();
 
-const s3 = new S3Client({ region: process.env.AWS_REGION, 
-    credentials:{accessKeyId: process.env.AWS_ACCESS_KEY, 
-                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY} });
-const dynamoDB = new DynamoDBClient({ region: process.env.AWS_REGION, 
-    credentials: {accessKeyId: process.env.AWS_ACCESS_KEY, 
-                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY} });
+const s3 = new S3Client({ region: process.env.AWS_REGION });
+const client = new DynamoDBClient({ region: process.env.AWS_REGION });
+const dynamoDB = DynamoDBDocumentClient.from(client);
 
-const awsCreds = {
-    aws_project_region: "us-west-1",
-    aws_cognito_region: "us-west-1",
-    aws_user_pools_id: "us-east-1_ATVGJk4CW",
-    aws_user_pools_web_client_id:"7jubasd14o6kf5co7vhvkavsbm"
-};
-
-export {s3, dynamoDB, awsCreds};
+export {s3, dynamoDB};
 
