25f98641ecc3beb0a7daf3ebd60d4d9143b8d8b5
Wen0131 <144382637+Wen0131@users.noreply.github.com>
2025-03-08 14:22:46 -0800

    lambda function to handle methods for CRUD in API gatway

--- backend/lambdaHandler.js
+++ backend/lambdaHandler.js
@@ -1,17 +1,79 @@
-import {gethUserList } from "./awsServices/dynamoDB.js";
 
-exports.handler = async (event) => {
-    try {
-        const users = await gethUserList();
-        return {
-            statusCode: 200,
-            body: JSON.stringify(users),
-            headers: {"Content-Type": "application/json"}
-        }
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({error: "Could not retrieve user data"})
-        }
+import {checkUserExists,
+   addUserToTable, 
+   getUserList, 
+   deleteUser,
+   updateUserInfo,
+   getUserByID} from "./awsServices/dynamoDB.js";
+
+export const handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  try {
+    console.log("Received event:", JSON.stringify(event, null, 2));
+    if (event.triggerSource === "PostConfirmation_ConfirmSignUp") {
+        return await postSignUpTrigger(event)
     }
+    switch (`${event.httpMethod} ${event.resource}`) { //.routekey is for HTTP API, use .httpMethod and .resource for REST API e.g., "GET" and "/Users"
+      case "DELETE /User/{id}":
+        body = await deleteUser(event.pathParameters.id)
+        break;
+
+      case "GET /User/{id}":
+        body = await getUserByID(event.pathParameters.id)
+        break;
+
+      case "GET /Users":
+        body = await getUserList(); 
+        break;
+
+      case "PUT /User":
+        const {userID, firstName, lastName, email } = JSON.parse(event.body);
+        body = await updateUserInfo(userID, {firstName, lastName, email});
+        break;
+
+      default:
+        throw new Error(`Unsupported route: "${event.routeKey}"`);
+    }
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
+
+};
+
+export async function postSignUpTrigger(event) {
+    console.log("Post Sign-Up confirmation");
+
+    const userID = event.request.userAttributes.sub;
+    const email = event.request.userAttributes.email;
+    const firstName = event.request.userAttributes.given_name || "";
+    const lastName = event.request.userAttributes.family_name || "";
+
+    if (!userID || !email ) {
+        console.error("Missing required user attributes");
+        return event;
+    }
+
+    try {
+       await addUserToTable(userID, firstName, lastName, email);
+       console.log(`User ${userID} added to DynamoDB.` );
+    } catch (error) {
+        console.error("Error in postSignUpTrigger:", error);
+        throw error;
+    }
+
+    return event;
 }
