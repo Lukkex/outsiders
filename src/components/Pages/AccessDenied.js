cb7f75ecc111e38ebdcead847da92c366fc03fa0
Lukkex <87504872+Lukkex@users.noreply.github.com>
2025-03-11 16:57:09 -0700

    Added redirect to login if not signed-in, updated access-denied page, updated admin reroute util

--- src/components/Pages/AccessDenied.js
+++ src/components/Pages/AccessDenied.js
@@ -9,8 +9,7 @@ function AccessDenied() {
                 
                 <div class="main-container text-white text-center xl:text-4xl lg:text-3xl md:text-2xl sm:text-1xl xs:textl">
                     <div class="text-3xl cyan-gradient border border-gray-200 p-8 rounded-3xl shadow-lg shadow-cyan-900 bg-cyan-600">
-                        
-                        <p>Page Unavailable: Access Denied.</p>
+                        <p>ERROR 403: Access Denied.</p>
                     </div>
                 </div>
             </div>
