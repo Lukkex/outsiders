91e732a2b4c6239bb00731e4e1c77fa17c41c1d0
shawnee-porkat <shawneepor@live.com>
2025-03-25 01:38:05 -0700

    AA-76: forgot password option + functionality, log out fix

--- src/components/SignOutButton.js
+++ src/components/SignOutButton.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { signOut } from '@aws-amplify/auth';
 import { Link, useNavigate } from 'react-router-dom';
+import { useUser } from '../context/UserContext';
 
 function SignOutButton() {
     const navigate = useNavigate();
+    const { refreshUserData } = useUser();
 
     const handleSignOut = async () => {
         try {
-            await signOut();
-            navigate('/login'); // Redirect to login page after sign out
+            await signOut({ global: true });
+            
+            refreshUserData();
+            
+            window.location.href = '/login';
         } catch (error) {
             console.error('Error signing out:', error);
         }
