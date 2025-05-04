05263b062310adfbbfd53153232b5dc1aa10259b
Asephera <sonofgeek@hotmail.com>
2025-05-04 03:08:06 -0700

    remove display of footer on scheduling / forms page

--- src/utils/SiteContainer.js
+++ src/utils/SiteContainer.js
@@ -2,19 +2,15 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import SiteHeader from "./SiteHeader";
 import SiteFooter from "./SiteFooter";
+import { useLocation } from 'react-router-dom';
 
 function SiteContainer({content}) {
     return (
         <div className="site-container flex flex-col min-h-screen">
-            <header className="w-full">
-                <SiteHeader />
-            </header>
-            <main className="flex-1 pb-[80px]">
-                {content}
-            </main>
-            <footer className="w-full">
-                <SiteFooter />
-            </footer>
+            <header className="w-full"> <SiteHeader /> </header>
+            <main className="flex-1"> {content} </main>
+            {!(useLocation().pathname == "/registration") && <footer className="w-full"> <SiteFooter /> </footer>}
+            
         </div>
     );
 }
